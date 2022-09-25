9eb1f41fea0d350b7b5202c1e4a0155fbaae3be8
Borja <boorjabs@gmail.com>
2022-09-25 17:41:26 +0200

    Initial commit

---
+++ src/Kata.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Kata {
+
+    public static List<Object> filterList(final List<Object> list) {
+
+        return list.stream().filter(s -> {
+            return !(s instanceof String);
+        }).collect(Collectors.toList());
+
+    }
+}
